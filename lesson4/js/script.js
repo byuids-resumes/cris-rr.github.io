1676e0b0707b3f5ce425b239e456a25889bda3a7
cris-rr <crisr@byui.edu>
2020-02-02 20:19:55 -0500

    remove comments form script

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,22 +1,19 @@
-function toggleMenu() {
-    document.getElementsByClassName("menubar")[0].classList.toggle("hide");
-    // let currentText = document.getElementsByClassName("menuText")[0].innerHTML;
-    let menuText = document.getElementsByClassName("menuText")[0]
-    console.log(menuText.innerHTML);
-    if (menuText.innerHTML == "☰ Menu") {
-        console.log("entro 1")
-        menuText.innerHTML = "X";
-    } else {
-        console.log("entro 2")
-        menuText.innerHTML = "&#9776; Menu";
-    }
-}
-
 let options = {
     day:"numeric",
     month:"numeric",
     year:"numeric"
 };
 let lastDate = new Date();
-document.getElementById('lastDate').textContent = lastDate.toLocleDateString("en-US", options);
+document.getElementById('lastDate').textContent = lastDate.toLocaleDateString("en-US", options);
 document.getElementById('lastTime').textContent = lastDate.toLocaleTimeString("en-US", {hour12:false});
+
+function toggleMenu() {
+    document.getElementsByClassName("menubar")[0].classList.toggle("hide");
+    let menuText = document.getElementsByClassName("menuText")[0]
+    if (menuText.innerHTML == "☰ Menu") {
+        menuText.innerHTML = "X";
+    } else {
+        menuText.innerHTML = "&#9776; Menu";
+    }
+}
+
