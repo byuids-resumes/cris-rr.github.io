4c6668bbbe3221e64155103e68d58686773fd7a8
cris-rr <crisr@byui.edu>
2020-02-05 22:12:56 -0500

    Favicon and other fixes

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,11 +1,44 @@
 let options = {
-    day:"numeric",
-    month:"numeric",
-    year:"numeric"
+    day: "numeric",
+    month: "numeric",
+    year: "numeric"
 };
 let lastDate = new Date();
-document.getElementById('lastDate').textContent = lastDate.toLocaleDateString("en-US", options);
-document.getElementById('lastTime').textContent = lastDate.toLocaleTimeString("en-US", {hour12:false});
+let day = lastDate.getDay();
+let month = lastDate.getMonth();
+let year = lastDate.getFullYear();
+let dayMonth = lastDate.getDate()
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+let months = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August",
+    "September", 
+    "October", 
+    "November", 
+    "December"
+]
+
+let actualDate = days[day] + ", " + dayMonth + " " + months[month] + " " + year;
+document.getElementById('lastDate').textContent = actualDate;
+//document.getElementById('lastDate').textContent = lastDate.toLocaleDateString("en-US", options);
+// document.getElementById('lastTime').textContent = lastDate.toLocaleTimeString("en-US", {
+//     hour12: false
+// });
 
 function toggleMenu() {
     document.getElementsByClassName("menubar")[0].classList.toggle("hide");
@@ -16,4 +49,3 @@ function toggleMenu() {
         menuText.innerHTML = "&#9776; Menu";
     }
 }
-
